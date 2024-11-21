af5dd9d7aeb33817340aab9eeb7cb2f94735d0b5
AraceliHT <araceli@pull.srl>
2024-11-21 18:31:47 -0400

    Ejercicios con if

--- app/Http/Controllers/ExercisesController.php
+++ app/Http/Controllers/ExercisesController.php
@@ -6,38 +6,149 @@ use Illuminate\Http\Request;
 
 class ExercisesController extends Controller
 {
-    //Ejercicio a): INTERCAMBIO DE VALORES
+    // Concatenacion de mensajes con echo
+    public function concat()
+    {
+        $a = "1, ";
+        $a = $a . "2, ";
+        $a = $a . "3";
+        echo $a;
+    }
+    //Ejercicio a): IN;ERCAMBIO DE VALORES
     public function changeValues()
     {
-        $a = 7;
-        $b = 8;
-        echo "VALORES INICIALES:";
-        echo "<br>";
-        echo "a=" . $a;
-        echo "<br>";
-        echo "b=" . $b;
-        echo "<br>";
+        $a = 5;
+        $b = 6;
+        $text = "VALORES INICIALES:";
+        $text = $text . "<br>";
+        $text = $text . "a=" . $a;
+        $text = $text . "<br>";
+        $text = $text . "b=" . $b;
+        $text = $text . "<br>";
+        //echo $text;
 
         $a = $a + $b;
         $b = $a - $b;
         $a = $a - $b;
 
-        echo "VALORES CAMBIADOS";
-        echo "<br>";
-        echo "a=" . $a;
-        echo "<br>";
-        echo "b=" . $b;
+        $text = $text . "VALORES CAMBIADOS";
+        $text = $text . "<br>";
+        $text = $text . "a=" . $a;
+        $text = $text .  "<br>";
+        $text = $text . "b=" . $b;
+        echo $text;
+    }
+
+    //Ejercicio a) Forma 2:
+    public function chngValues()
+    {
+        $a = 15;
+        $b = 66;
+        $text = "VALORES INICIALES:";
+        $text = $text . "<br>";
+        $text = $text . "a=" . $a;
+        $text = $text . "<br>";
+        $text = $text . "b=" . $b;
+        $text = $text . "<br>";
+        //echo $text;
+        $c = $a;
+        $a = $b;
+        $b = $c;
+        $text = $text . "VALORES CAMBIADOS";
+        $text = $text . "<br>";
+        $text = $text . "a=" . $a;
+        $text = $text . "<br>";
+        $text = $text . "b=" . $b;
+        echo $text;
     }
 
     // Ejercicio b): MAYOR Y MENOR DE DOS NUM.
     public function dosNum(Request $request)
     {
-        //
+        $a = 5;
+        $b = 6;
+        $text = "1er num:  " . $a;
+        $text = $text . "<br>";
+        $text = $text . "2do num: " . $b;
+        $text = $text . "<br>";
+
+        $text = $text . "Comparacion:";
+        $text = $text . "<br>";
+
+        if ($a > $b) {
+            $text = $text . "El mayor = " . $a;
+            $text = $text . "<br>";
+            $text = $text . "El menor = " . $b;
+        } else {
+            $text = $text . "El mayor = " . $b;
+            $text = $text . "<br>";
+            $text = $text . "El menor = " . $a;
+        }
+        echo $text;
     }
 
     //Ejercicio c): MAYOR Y MENOR DE TRES NUM.
     public function tresNum()
     {
-        //
+        $a = 7;
+        $b = 5;
+        $c = 34;
+        $text = "1er num:  " . $a;
+        $text = $text . "<br>";
+        $text = $text . "2do num: " . $b;
+        $text = $text . "<br>";
+        $text = $text . "3er num: " . $c;
+        $text = $text . "<br>";
+        $text = $text . "<br>";
+        $text = $text . "Comparacion:";
+        $text = $text . "<br>";
+        $text = $text . "<br>";
+
+        if ($a > $b) {
+            if ($a > $c) {
+                if ($b > $c) {
+                    $text = $text . $a . " - " . $b . " - " . $c;
+                    $text = $text . "<br>";
+                    $text = $text . "El mayor es: " . $a;
+                    $text = $text . "<br>";
+                    $text = $text . "El menor es: " . $c;
+                } else {
+                    $text = $text . $a . " - " . $c . " - " . $b;
+                    $text = $text . "<br>";
+                    $text = $text . "El mayor es: " . $a;
+                    $text = $text . "<br>";
+                    $text = $text . "El menor es: " . $b;
+                }
+            } else {
+                $text = $text . $c . " - " . $a . " - " . $b;
+                $text = $text . "<br>";
+                $text = $text . "El mayor es: " . $c;
+                $text = $text . "<br>";
+                $text = $text . "El menor es: " . $b;
+            }
+        } else {
+            if ($b < $c) {
+                $text = $text . $c . " - " . $b . " - " . $a;
+                $text = $text . "<br>";
+                $text = $text . "El mayor es: " . $c;
+                $text = $text . "<br>";
+                $text = $text . "El menor es: " . $a;
+            } else {
+                if ($a < $c) {
+                    $text = $text . $b . " - " . $c . " - " . $a;
+                    $text = $text . "<br>";
+                    $text = $text . "El mayor es: " . $b;
+                    $text = $text . "<br>";
+                    $text = $text . "El menor es: " . $a;
+                } else {
+                    $text = $text . $b . " - " . $a . " - " . $c;
+                    $text = $text . "<br>";
+                    $text = $text . "El mayor es: " . $b;
+                    $text = $text . "<br>";
+                    $text = $text . "El menor es: " . $c;
+                }
+            }
+        }
+        echo $text;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Exercises2Controller;
 use App\Http\Controllers\ExercisesController;
 use Illuminate\Support\Facades\Route;
 
@@ -7,4 +8,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/changeValues', [ExercisesController::class, 'changeValues']);
+Route::get('/tresNum', [ExercisesController::class, 'tresNum']);
+Route::get('/chngValues', [ExercisesController::class, 'chngValues']);
 Route::get('/dosNum', [ExercisesController::class, 'dosNum']);
+Route::get('/concat', [ExercisesController::class, 'concat']);
+
+Route::get('/for1', [Exercises2Controller::class, 'for1']);
